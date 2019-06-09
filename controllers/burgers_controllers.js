a3fcda7bba5871ec4a17c1c12ad38f5665a816ca
dtirado <dtirado80@msn.com>
2019-06-08 23:43:28 -0500

    some changes in logic and js

--- controllers/burgers_controllers.js
+++ controllers/burgers_controllers.js
@@ -10,12 +10,13 @@ var burger = require("../models/burger.js");
 router.get("/", function(req, res) {
     burger.all(function(data) {
         var hbsObject = {
-            burgers: data
+            burger: data
         };
         console.log(hbsObject);
         res.render("index", hbsObject);
     });
 });
+
 
 // when "/api/burgers" url is hit burgers are updated
 router.post("/api/burgers", function(req, res) {
@@ -29,21 +30,24 @@ router.post("/api/burgers", function(req, res) {
     );
 });
 
-router.put("/api/burgers/:id", function (req, res) {
-	var condition = "id = " + req.params.id;
-
-	console.log("condition", condition);
-
-	burger.update({
-		devoured: req.body.devoured
-	}, condition, function (result) {
-		if (result.changedRows == 0) {
-			// If no rows were changed, then the ID must not exist, so 404
-			return res.status(404).end();
-		} else {
-			res.status(200).end();
-		}
-	});
+router.put("/api/burgers/:id", function(req, res) {
+    console.log("id = " + req.params.id);
+    burger.update(req.params.id, function(){
+      res.sendStatus(200);
+    })
+    // burger.update(
+    //     {
+    //         devoured: req.body.devoured
+    //     },
+    //     function(result) {
+    //         if (result.changedRows == 0) {
+    //             // If no rows were changed, then the ID must not exist, so 404
+    //             return res.status(404).end();
+    //         } else {
+    //             res.status(200).end();
+    //         }
+    //     }
+    // );
 });
 
 router.delete("/api/burgers/:id", function(req, res) {
