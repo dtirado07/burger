2f6ac9ecead84775e945e145f6483988f298e494
dtirado <dtirado80@msn.com>
2019-06-30 00:03:22 -0600

    Heroku troubleshooting

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+
+INSERT INTO burgers (burger_name, devoured) VALUES ('Bacon Cheeseburger', 0);
+
+INSERT INTO burgers (burger_name, devoured) VALUES ('Angus Burger', 0);
+
+INSERT INTO burgers (burger_name, devoured) VALUES ('Teriyaki burger', 0);
